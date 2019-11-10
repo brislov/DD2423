35b0a9aca95bd816c35e173ee0e772ca894786e2
Markus Brislöv <markusbrislov@gmail.com>
2019-11-10 18:50:03 +0100

    on q13

--- lab1/script.m
+++ lab1/script.m
@@ -1,5 +1,7 @@
 clear
 
+% 1 Properties of the discrete Fourier transform
+
 % Questions 1-6
 % pValues = [5 9 17 17 5 125];
 % qValues = [9 5 9 121 1 1];
@@ -14,17 +16,17 @@ clear
 % end
 
 
-% Questions 7-
-sz = 128;
-
-F = [zeros(56, sz); ones(16, sz); zeros(56, sz)];
-G = F';
-H = F + 2 * G;
-
-Fhat = fft2(F);
-Ghat = fft2(G);
-Hhat = fft2(H);
-
+% Questions 7-9
+% sz = 128;
+% 
+% F = [zeros(56, sz); ones(16, sz); zeros(56, sz)];
+% G = F';
+% H = F + 2 * G;
+% 
+% Fhat = fft2(F);
+% Ghat = fft2(G);
+% Hhat = fft2(H);
+% 
 % subplot(331)
 % showgrey(F)
 % subplot(334)
@@ -43,12 +45,88 @@ Hhat = fft2(H);
 % subplot(339)
 % showgrey(log(1 + abs(fftshift(Hhat))))
 
-subplot(221)
-showgrey(F.*G)
-subplot(223)
-showfs(fft2(F.*G))
-subplot(222)
-% showgrey(ifft2(conv2(Fhat,Ghat)))
-subplot(224)
-showfs(fftshift(conv2(Fhat,Ghat)/sz))
+
+% Question 10 
+% figure(1)
+% subplot(121)
+% showgrey(F.*G)
+% title('F.*G')
+% subplot(122)
+% showfs(fft2(F.*G))
+% title('fft2(F.*G)')
+% 
+% figure(2)
+% subplot(121)
+% c = conv2(Fhat, Ghat)/sz.^2;
+% showfs(c(1:sz, 1:sz));
+% title('conv2(Fhat, Ghat)/sz.^2')
+
+
+% Question 11
+% F2 = [zeros(60, 128); ones(8, 128); zeros(60, 128)] .* ...
+%      [zeros(128, 48) ones(128, 32) zeros(128, 48)];
+% 
+% subplot(221)
+% showgrey(F.*G)
+% subplot(222)
+% showfs(fft2(F.*G))
+% subplot(223)
+% showgrey(F2)
+% subplot(224)
+% showfs(fft2(F2))
+
+
+% Question 12
+% i = 0;
+% alphas = [0 30 45 60 90];
+% for alpha = alphas
+%     G = rot(F,alpha);
+%     
+%     i = i+1;
+%     subplot(length(alphas),3,i)
+%     showgrey(G)
+%     axis on
+%     title(sprintf('G, %i deg', alpha))
+%     
+%     i = i+1;
+%     subplot(length(alphas),3,i)
+%     Ghat = fft2(G);
+%     showfs(fft2(G))
+%     title(sprintf('Ghat, %i deg', alpha))
+%     
+%     i = i+1;
+%     subplot(length(alphas),3,i)
+%     Hhat = rot(fftshift(Ghat),-alpha);
+%     showgrey(log(1+abs(Hhat)))
+%     title(sprintf('Hhat, 0 deg'))
+% end 
+
+
+% Question 12
+% a = 10^-10;
+% 
+% i = 0;
+% titles = {'phonelcalc128' 'few128' 'nallo128'};
+% images = {phonecalc128 few128 nallo128};
+% for m = 1:length(images)
+%     i = i+1;
+%     subplot(length(images),3,i)
+%     showgrey(images{m})
+%     title(titles{m})
+%     
+%     i = i+1;
+%     subplot(length(images),3,i)
+%     showgrey(pow2image(images{m},a))
+%     title('pow2image()')
+%     
+%     i = i+1;
+%     subplot(length(images),3,i)
+%     showgrey(randphaseimage(images{m}))
+%     title('randphaseimage()')
+% end
+
+
+
+% 2 Gaussian convolution implemented via FFT
+
 

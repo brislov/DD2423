18701ce995c8c15494c507628e449c27ae4f4c31
Markus Brislöv <markusbrislov@gmail.com>
2019-11-13 23:05:26 +0100

    pre-presentation

--- lab1/script.m
+++ lab1/script.m
@@ -17,16 +17,16 @@ clear
 
 
 % Questions 7-9
-sz = 128;
+% sz = 128;
 % 
-F = [zeros(56, sz); ones(16, sz); zeros(56, sz)];
-G = F';
+% F = [zeros(56, sz); ones(16, sz); zeros(56, sz)];
+% G = F';
 % H = F + 2 * G;
 % 
-Fhat = fft2(F);
-Ghat = fft2(G);
+% Fhat = fft2(F);
+% Ghat = fft2(G);
 % Hhat = fft2(H);
-% 
+
 % subplot(331)
 % showgrey(F)
 % subplot(334)
@@ -47,19 +47,19 @@ Ghat = fft2(G);
 
 
 % Question 10 
-figure(1)
-subplot(121)
-showgrey(F.*G)
-title('F.*G')
-subplot(122)
-showfs(fft2(F.*G))
-title('fft2(F.*G)')
-
-figure(2)
-subplot(121)
-c = conv2(Fhat, Ghat)/sz.^2;
-showfs(c(1:sz, 1:sz));
-title('conv2(Fhat, Ghat)/sz.^2')
+% figure(1)
+% subplot(121)
+% showgrey(F.*G)
+% title('F.*G')
+% subplot(122)
+% showfs(fft2(F.*G))
+% title('fft2(F.*G)')
+% 
+% figure(2)
+% subplot(121)
+% c = conv2(Fhat, Ghat)/sz.^2;
+% showfs(c(1:sz, 1:sz));
+% title('conv2(Fhat, Ghat)/sz.^2')
 
 
 % Question 11
@@ -163,12 +163,11 @@ title('conv2(Fhat, Ghat)/sz.^2')
 % 3 Smoothing
 
 % Questions 17-18
-office = office256;
+% office = office256;
+% 
+% add = gaussnoise(office,16);
+% sap = sapnoise(office,0.1,255);
 
-add = gaussnoise(office,16);
-sap = sapnoise(office,0.1,255);
-
-% figure(1)
 % subplot(131)
 % showgrey(office)
 % title('no added noise')
@@ -204,12 +203,12 @@ sap = sapnoise(office,0.1,255);
 % 
 % subplot(nrow,ncol,i)
 % showgrey(img)
-% title('sap')
+% title('add')
 % 
 % for t = [1.0 4.0 16.0 64.0 256]
 %     i = i+1;
 %     subplot(nrow,ncol,i)
-%     showgray(gaussfft(add,t))
+%     showgray(gaussfft(img,t))
 %     title(sprintf('t = %.1f',t))
 % end
 
@@ -224,15 +223,15 @@ sap = sapnoise(office,0.1,255);
 % showgrey(img)
 % title('sap')
 % 
-% for m = [2 4 8 16 32]
+% for m = [2 4 6 12 24]
 %     i = i+1;
 %     subplot(nrow,ncol,i)
-%     showgray(medfilt(add,m))
+%     showgray(medfilt(img,m))
 %     title(sprintf('m = %i',m))
 % end
 
 % Ideal low-pass filter
-% img = sap;
+% img = add;
 % 
 % i = 1;
 % ncol = 3;
@@ -240,9 +239,9 @@ sap = sapnoise(office,0.1,255);
 % 
 % subplot(nrow,ncol,i)
 % showgrey(img)
-% title('sap')
+% title('add')
 % 
-% for cutoffFreq = [0.05 0.1 0.15 0.2 0.25]
+% for cutoffFreq = [0.05 0.15 0.2 0.25 0.35]
 %     i = i+1;
 %     subplot(nrow,ncol,i)
 %     showgrey(ideal(img,cutoffFreq,'l'))
@@ -256,6 +255,7 @@ sap = sapnoise(office,0.1,255);
 % smoothimgideal = img;
 % N=5;
 % for i=1:N
+%     figure(1)
 %     if i>1 % generate subsampled versions
 %         img = rawsubsample(img);
 %         smoothimggauss = gaussfft(smoothimggauss, 1);% <call_your_filter_here>(smoothimg, <params>);
@@ -269,4 +269,14 @@ sap = sapnoise(office,0.1,255);
 %     showgrey(smoothimggauss)
 %     subplot(3, N, i+N+N)
 %     showgrey(smoothimgideal)
+%     
+%     if i==4
+%         figure(2)
+%         subplot(131)
+%         showgrey(img)
+%         subplot(132)
+%         showgrey(smoothimggauss)
+%         subplot(133)
+%         showgrey(smoothimgideal)
+%     end
 % end

c743b68374e5741f50ee97558c2e5f407de1c246
Markus Brislöv <markusbrislov@gmail.com>
2019-12-03 20:20:28 +0100

    k-means++

--- lab3/kmeans_example.m
+++ lab3/kmeans_example.m
@@ -1,7 +1,4 @@
-% img = imread('orange.jpg');
-% img = imread('tiger1.jpg');
-% img = imread('tiger2.jpg');
-img = imread('asd.jpg');
+clear
 
 K = 8;               % number of clusters used
 L = 10;              % number of iterations
@@ -9,24 +6,23 @@ seed = 14;           % seed used for random initialization
 scale_factor = 1.0;  % image downscale factor
 image_sigma = 1.0;   % image preblurring scale
 
-
-I = img;
+% I = imread('orange.jpg');
+% I = imread('tiger1.jpg');
+% I = imread('tiger2.jpg');
+I = imread('tiger3.jpg');
 I = imresize(I, scale_factor);
 Iback = I;
 d = 2*ceil(image_sigma*2) + 1;
 h = fspecial('gaussian', [d d], image_sigma);
 I = imfilter(I, h);
 
-
 tic
 [ segm, centers ] = kmeans_segm(I, K, L, seed);
 toc
 Inew = mean_segments(Iback, segm);
 Iob = overlay_bounds(Iback, segm);
-
 imwrite(Inew,'result/kmeans1.png')
 imwrite(Iob,'result/kmeans2.png')
-
 
 subplot(131)
 imshow(I)
--- lab3/kmeans_segm.m
+++ lab3/kmeans_segm.m
@@ -5,9 +5,53 @@ function [segmentation, centers] = kmeans_segm(image, K, L, seed)
 [nrows, ncols] = size(image, 1, 2);
 image = double(reshape(image, [nrows*ncols 3]));
 
+
+% The exact algorithm is as follows:
+% 
+%   1. Choose one center uniformly at random from among the data points.
+%   2. For each data point x, compute D(x), the distance between x and the 
+%      nearest center that has already been chosen.
+%   3. Choose one new data point at random as a new center, using a 
+%      weighted probability distribution where a point x is chosen with 
+%      probability proportional to D(x)^2.
+%   4. Repeat Steps 2 and 3 until k centers have been chosen.
+%   5. Now that the initial centers have been chosen, proceed using 
+%      standard k-means clustering.
+
+rng(seed)
+centers = zeros(K, 3);
+centers(1, :) = image(randi(nrows*ncols));
+
+for k = 2:K
+    
+    % compute distance between x and the closest centroid
+    if k == 2
+        D2 = pdist2(centers(1, :), image, 'squaredeuclidean');
+    else
+        D2 = min(pdist2(centers(1:k-1, :), image, 'squaredeuclidean'));
+    end
+    
+    probs = D2 / sum(D2);
+    cumprobs = cumsum(probs);
+   
+    t = rand();
+    [~, I] = max(cumprobs >= t); % gives first index where this is true
+    
+    centers(k, :) = image(I, :);
+end 
+
+
 % Randomly initialize the K cluster centers
-rng(seed);
-centers = randi([0 255], [K 3]);
+% centers2 = randi([0 255], [K 3]);
+
+
+figure(1)
+subplot(121)
+plot3(image(:, 1), image(:, 2), image(:, 3), 'o')
+subplot(122)
+plot3(centers(:, 1), centers(:, 2), centers(:, 3), 'o')
+
+
 
 % Compute all distances between pixels and cluster centers
 distances = pdist2(centers, image);

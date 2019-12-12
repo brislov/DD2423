ad188490955b3f7bf6e0abe1e69396ca8178369e
Markus Brislöv <markusbrislov@gmail.com>
2019-12-12 15:05:29 +0100

    b4 pres 2

--- lab3/graphcut_example.m
+++ lab3/graphcut_example.m
@@ -1,25 +1,25 @@
 clear
 
-I = imread('tiger1.jpg');
-scale_factor = 0.5;             % image downscale factor
-area = [100 140 550 300];        % image region to train foreground with
-K = 3;                          % number of mixture components
-alpha = 8.0;                    % maximum edge cost
-sigma = 10.0;                   % edge cost decay factor
-
-% I = imread('tiger2.jpg');
+% I = imread('tiger1.jpg');
 % scale_factor = 0.5;             % image downscale factor
-% area = [160 80 430 250];        % image region to train foreground with
-% K = 2;                          % number of mixture components
-% alpha = 8.0;                    % maximum edge cost
-% sigma = 10.0;                   % edge cost decay factor
+% area = [100 140 550 300];       % image region to train foreground with
+% K = 16;                         % number of mixture components
+% alpha = 8;                      % maximum edge cost
+% sigma = 10;                     % edge cost decay factor
+
+I = imread('tiger2.jpg');
+scale_factor = 0.5;             % image downscale factor
+area = [160 80 410 220];        % image region to train foreground with
+K = 16;                          % number of mixture components
+alpha = 16;                    % maximum edge cost
+sigma = 4;                   % edge cost decay factor
 
 % I = imread('tiger3.jpg');
 % scale_factor = 0.5;             % image downscale factor
-% area = [200 100 450 250];        % image region to train foreground with
-% K = 1;                          % number of mixture components
-% alpha = 8.0;                    % maximum edge cost
-% sigma = 10.0;                   % edge cost decay factor
+% area = [200 100 450 250];       % image region to train foreground with
+% K = 16;                         % number of mixture components
+% alpha = 9;                    % maximum edge cost
+% sigma = 18;                     % edge cost decay factor
 
 
 I = imresize(I, scale_factor);
@@ -42,3 +42,5 @@ subplot(2,2,1); imshow(Inew);
 subplot(2,2,2); imshow(Iob);
 subplot(2,2,3); imshow(prior);
 subplot(224); imshow(imoverlay(I, mask));
+h = suptitle(sprintf('K=%i, alpha=%.1f, sigma=%.1f', K, alpha, sigma));
+set(h, 'FontSize', 12)
